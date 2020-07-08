e6089331ea95ce8e4118372464b7a0730540a7c9
KIMSIYOUNG <tldud2404@gmail.com>
2020-07-08 09:28:38 +0900

    feat : 위치에 따른 날씨 정보 보여주기 기능 구현

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Loading from './Loading';
-// `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
-// const API_KEY = "3147da487cd903f92848f0c5f86f1eee";
+import * as Location from "expo-location";
+import axios from "axios";
+import Weather from './Weather';
+
+const API_KEY = "3147da487cd903f92848f0c5f86f1eee";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [temp, setTemp] = useState(null);
+  const [condition, setCondition] = useState(null);
+
+  const getWeather = async (latitude, longitude) => {
+    const { data: {
+              main: { temp },
+              weather
+          }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    setIsLoading(false);
+    setTemp(temp);
+    setCondition(weather[0].main);
+  }
+
+  const requestLocation = async () => {
+    try {
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      getWeather(latitude, longitude);
+    }
+    catch (error) {
+      console.log("No Permission", "so sad");
+    }
+  }
+
+  useEffect(() => {
+    requestLocation();
+  }, []);
+
   return (
-    <Loading/>
+    isLoading ? <Loading/> : <Weather temp={Math.round(temp)} condition={condition}/>
   );
 }
 
